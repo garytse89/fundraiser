35a18991977e07c6f1425e9125226b63882d5d98
Gary Tse <garytse89@gmail.com>
2014-11-04 18:10:09 +0800

    main.html page should do a GET request for all projects (change in the future) from database

--- dist/app/scripts/controllers/main.js
+++ dist/app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('distApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.loadProjects = function() {
+    	$http.get('/projects').
+	    	success(function(data, status, headers, config) {
+	    		$scope.projects = data;
+	    	}).
+	    	error(function(err, status, headers, config) {
+	    		console.log(err)
+	    	});
+    };
+
+    $scope.loadProjects();
+});
