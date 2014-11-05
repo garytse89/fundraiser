fb2d00c3d680a843216d50ed24ba6e7f6f9215db
zacharynevin <zachary.nevin1@gmail.com>
2014-11-04 20:57:26 -0800

    pledge amount disables under 40K projects

--- dist/app/scripts/controllers/pledge.js
+++ dist/app/scripts/controllers/pledge.js
@@ -3,16 +3,27 @@
 angular.module('distApp')
   .controller('PledgeCtrl', ['$scope', '$routeParams', 'API', '$location', function($scope, $routeParams, API, $location) {
 
+  	var project_id = $routeParams.project_id;
+  	// amount field is enabled by default
+  	$scope.amountDisabled = false;
+
+  	API.project({ project_id: project_id }).$promise.then(function(project) {
+  		if (project.cost < 40000) {
+  			$scope.amount = 40000
+  			$scope.amountDisabled = true
+  		}
+  	})
+
   	$scope.fund = function() {
   		API.fund({ 
   			name: [$scope.first_name, $scope.lastname].join(' '),
   			email: $scope.email,
   			amount: $scope.amount,
-  			project_id: $routeParams.project_id 
+  			project_id: project_id 
   		}).$promise.then(function() {
   			// show indication that pledge worked
   			// then redirect back to project page
-  			$location.path('/#/projects/' + $routeParams.project_id)
+  			$location.path('/#/projects/' + project_id)
   		}, function(err) {
   			// show indication that pledge failed
   		})
