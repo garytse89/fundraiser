89560309e70c2a59849c09a80934c32c4b5457da
zacharynevin <zachary.nevin1@gmail.com>
2014-11-05 23:06:24 -0800

    integrated socketio and redis with app

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var http = require('http'),
     path = require('path'),
 	colors = require('colors'),
   	connect = require('connect'),
-  	frontEndDir = 'dist/app/'
+  	frontEndDir = 'dist/app/';
+
+ if (process.env.NODE_ENV !== 'production') {
+	process.env.REDIS_HOSTNAME = 'localhost'
+	process.env.REDIS_PORT = 6379
+	process.env.REDIS_AUTH = ''
+};
 
 app.set('views', path.join(__dirname, frontEndDir));
 app.set('view engine', 'ejs');
@@ -29,8 +35,25 @@ global.Models = require('./config/mongoose')('fundraising')
 
 require('./api')(app)
 
-http.createServer(app).listen(app.get('port'), function() {
+var server = http.createServer(app)
+
+server.listen(app.get('port'), function() {
   console.log('Express server for "Fundraiser" listening on port %s'.bold.green, app.get('port'));
 });
+
+var redis = require('redis')
+var io = require('socket.io')(server),
+    socketio_redis = require('socket.io-redis'),
+    pubClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, { auth_pass: process.env.REDIS_AUTH, return_buffers: true }),
+    subClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, { auth_pass: process.env.REDIS_AUTH, return_buffers: true });
+
+
+io.adapter(socketio_redis({ 
+  host: process.env.REDIS_HOSTNAME,
+  port: process.env.REDIS_PORT,
+  auth: process.env.REDIS_AUTH,
+  pubClient: pubClient,
+  subClient: subClient 
+}))
 
 module.exports = app
