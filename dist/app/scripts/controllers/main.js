b0e03104a68b518d600cdb5119a5af6d3837180a
zacharynevin <zachary.nevin1@gmail.com>
2014-11-07 11:59:09 -0800

    replaced numtoword with custom filter, misc fixes
    - added numToWords and toTitleCase filters

--- dist/app/scripts/controllers/main.js
+++ dist/app/scripts/controllers/main.js
@@ -8,12 +8,9 @@ angular.module('distApp')
     API.projects({ category: $scope.category }).$promise.then(function(projects) {
       $scope.projects = projects
       angular.forEach(projects, function(project) {
-        if (project.limit <= 0 ) {
-          project.funded = true
-        }
-        var numInWords = toWords(project.limit)
-        numInWords = toTitleCase(numInWords)
-        project.countRemaining = numInWords
+        
+        project.funded = p.limit <= 0
+
       })
     })
 
@@ -24,19 +21,20 @@ angular.module('distApp')
    		$scope.categories = categories
 
    		$scope.project_count.all = API.countProjects()
+
    		angular.forEach(categories, function(category) {
    			$scope.project_count[category.name] = API.countProjects({ category: category.name })
    		})
+
    	})
 
    	$scope.switchCategory = function(category) {
    		API.projects({ category: category }).$promise.then(function(projects) {
         $scope.projects = projects
         angular.forEach(projects, function(project) {
-          if (project.limit <= 0 ) project.funded = true
-          var numInWords = toWords(project.limit)
-          numInWords = toTitleCase(numInWords)
-          project.countRemaining = numInWords
+          
+          project.funded = p.limit <= 0
+
         })
       })
    		$scope.current_category = category
@@ -47,17 +45,9 @@ angular.module('distApp')
 		    _($scope.projects).map(function(project) {
 		    	if (project._id == data.project_id) {
             API.project({ project_id: project._id }).$promise.then(function(p) {
-              if (p.limit <= 0) {
-  		    		  project.funded = true // if project had 1 donation count remaining
-              } else {
-                project.funded = false
-              }
+              
+              project.funded = p.limit <= 0
 
-              console.log('remaining limits = ', p.limit)
-              console.log('project.funded = ', project.funded)
-              var numInWords = toWords(p.limit)
-              numInWords = toTitleCase(numInWords)
-              project.countRemaining = numInWords
             })
 		    	}
 		    	return project
@@ -66,8 +56,3 @@ angular.module('distApp')
 	  })
 
 }]);
-
-function toTitleCase(str)
-{
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
