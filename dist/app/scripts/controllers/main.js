90ef0c5e50704eac5fa8fe8552a4ba7b8de0731b
Gary Tse <garytse89@gmail.com>
2014-11-10 11:13:54 +0800

    Added individual category progress bars to each category page Colors do not fill on a reload

--- dist/app/scripts/controllers/main.js
+++ dist/app/scripts/controllers/main.js
@@ -6,12 +6,14 @@ angular.module('distApp')
   	$scope.category = $routeParams.category
 
     API.projects({ category: $scope.category }).$promise.then(function(projects) {
-      $scope.projects = projects
+      $scope.projects = projects    
+
       angular.forEach(projects, function(project) {
-        
         project.funded = project.limit == 0
         if (project.limit <= -1) project.unlimited = true
-      })
+      })     
+
+      $scope.calculateTotals(projects)
     })
 
    	$scope.current_category = $scope.category
@@ -28,10 +30,41 @@ angular.module('distApp')
 
    	})
 
+    $scope.calculateTotals = function(projects) {
+
+      // pre-calculated totals
+      $scope.total_rescue = 116645
+      $scope.total_rehabilitation = 340280
+      $scope.total_investment = 511850
+
+      // calculate total so far 
+      var sum_rescue = 0
+      var sum_rehabilitation = 0
+      var sum_investment = 0
+
+      angular.forEach(projects, function(project) {  
+        if (project.limit > 0) { // don't bother adding in the unlimited projects
+          if (project.category === "Rescue") {
+            sum_rescue += project.cost * project.limit
+          } else if (project.category === "Rehabilitation") {
+            sum_rehabilitation += project.cost * project.limit
+          } else {
+            sum_investment += project.cost * project.limit
+          }
+        }
+      })
+
+      $scope.current_rescue = $scope.total_rescue - sum_rescue
+      $scope.current_rehabilitation = $scope.total_rehabilitation - sum_rehabilitation
+      $scope.current_investment = $scope.total_investment - sum_investment
+    }
+
    	$scope.switchCategory = function(category) {
    		API.projects({ category: category }).$promise.then(function(projects) {
         $scope.current_category = category
         $scope.projects = projects
+
+        $scope.calculateTotals(projects)
       })
    	}
 
@@ -49,6 +82,12 @@ angular.module('distApp')
 
   	Socket.on('project::funded', function(data) {
       console.log(data)
+
+      API.projects().$promise.then(function(projects) {
+        $scope.projects = projects   
+        $scope.calculateTotals(projects)
+      })
+
   		$scope.$apply(function() {
 		    _($scope.projects).map(function(project) {
 		    	if (project._id == data.project_id) {
